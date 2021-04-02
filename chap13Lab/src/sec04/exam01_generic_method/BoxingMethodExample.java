d22e478aa52954b2dfbc4cadbdf85d561920a966
leedorry <iklesd@gmail.com>
2021-04-02 15:03:12 +0900

    chap13 (2021.04.02)

--- chap13Lab/src/sec04/exam01_generic_method/BoxingMethodExample.java
+++ chap13Lab/src/sec04/exam01_generic_method/BoxingMethodExample.java
@@ -3,9 +3,11 @@ package sec04.exam01_generic_method;
 public class BoxingMethodExample {
 	public static void main(String[] args) {
 		Box<Integer> box1 = Util.<Integer>boxing(100);
-		int intValue = box1.get();
+		int intValue = box1.get();  // Unboxing, int <- Integer
+		System.out.println("intValue = "+ intValue);
 		
-		Box<String> box2 = Util.boxing("홍길동");
+		Box<String> box2 = Util.boxing("홍길동");  // 제너릭 메소드 호출한다.
 		String strValue = box2.get();
+		System.out.println("strValue = "+ strValue);
 	}
 }
