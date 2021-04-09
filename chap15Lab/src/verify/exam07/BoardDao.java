789f886b2c136dae2460b4e02f74fd5c2e364fbe
leedorry <iklesd@gmail.com>
2021-04-09 11:36:38 +0900

    chap15 (2021.04.09)

---
+++ chap15Lab/src/verify/exam07/BoardDao.java
@@ -0,0 +1,16 @@
+package verify.exam07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// DAO : Data Access Object, 데이터 접근 객체
+public class BoardDao {
+	// 작성 위치
+	public List<Board> getBoardList(){
+		List<Board> list = new ArrayList<Board>();
+		list.add(new Board("제목1", "내용1"));
+		list.add(new Board("제목2", "내용2"));
+		list.add(new Board("제목3", "내용3"));
+		return list;
+	}
+}
---
+++ chap15Lab/src/verify/exam09/Student.java
@@ -0,0 +1,34 @@
+package verify.exam09;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String,Integer> map = new HashMap<String,Integer>();
+		map.put("blue", 96);
+		map.put("hong", 86);
+		map.put("white", 92);
+		String name = null;
+		int maxScore = 0;
+		int totalScore = 0;
+		// #1 작성 위치
+		
+		
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+}
