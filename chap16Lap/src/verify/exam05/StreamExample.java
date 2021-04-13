c1f9a0976ab52f8864172ebf71ae426b40886e91
leedorry <iklesd@gmail.com>
2021-04-13 10:38:57 +0900

    chap16 (2021.04.13)

---
+++ chap16Lap/src/verify/exam05/StreamExample.java
@@ -0,0 +1,20 @@
+package verify.exam05;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExample {
+
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList(
+				"This is a java book",
+				"Lambda Expressions",
+				"Java8 supports lambda expressions"
+				);
+				list.stream()
+				//#1
+				.filter(a -> a.toLowerCase().contains("java"))
+				.forEach(a -> System.out.println(a));
+	}
+
+}
