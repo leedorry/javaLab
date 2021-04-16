d86f436168df18a419982c0baa29b74954cfcbe7
leedorry <iklesd@gmail.com>
2021-04-16 12:47:28 +0900

    chap18 (2021.04.16)

--- chap18Lab/src/sec05/exam01_inputstreamreader/InputStreamReaderExample.java
+++ chap18Lab/src/sec05/exam01_inputstreamreader/InputStreamReaderExample.java
@@ -6,7 +6,9 @@ import java.io.Reader;
 
 public class InputStreamReaderExample {
 	public static void main(String[] args) throws Exception {
+		// 바이트기반 입력스트림
 		InputStream is = System.in;
+		// 보조스트림을 사용하여 문자기반 입력스트림으로 변환한다.
 		Reader reader = new InputStreamReader(is);	
 		
 		int readCharNo;
